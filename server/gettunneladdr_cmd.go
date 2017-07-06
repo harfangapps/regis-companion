ceaa502bfd42c90553a63ccb960ccc1fa6ba2d25
Martin Angers <martin.n.angers@gmail.com>
2017-07-06 15:00:54 -0400

    server: work on parsing GETTUNNELADDR arguments

--- server/gettunneladdr_cmd.go
+++ server/gettunneladdr_cmd.go
@@ -1,7 +1,10 @@
 package server
 
 import (
+	"errors"
 	"fmt"
+	"net"
+	"strconv"
 
 	"bitbucket.org/harfangapps/regis-companion/resp"
 )
@@ -15,7 +18,51 @@ func (c getTunnelAddrCmd) Execute(cmdName string, req []string, s *Server) (inte
 	}
 
 	// SSH server address, default port to 22
+	serverAddr, err := parseAddr(req[1], 22)
+	if err != nil {
+		return resp.Error(fmt.Sprintf("ERR invalid SSH server address: %s", err)), nil
+	}
+	// remote address, port required
+	remoteAddr, err := parseAddr(req[2], 0)
+	if err != nil {
+		return resp.Error(fmt.Sprintf("ERR invalid remote server address: %s", err)), nil
+	}
 
-	// Remote server address, port required
 	return nil, nil
+}
+
+// TODO: should probably not use net.Addr, just a string
+func parseAddr(s string, defaultPort int) (net.Addr, error) {
+	host, port, err := net.SplitHostPort(s)
+	if err != nil {
+		// if port is required, return that error
+		if defaultPort <= 0 {
+			return nil, err
+		}
+
+		// not host:port, try host only
+		ip := net.ParseIP(s)
+		if ip == nil {
+			// not ip, unsupported address
+			return nil, err
+		}
+
+		return &net.TCPAddr{IP: ip, Port: defaultPort}, nil
+	}
+
+	// TODO: should allow host names too, not just IP addresses...
+	ip := net.ParseIP(host)
+	if ip == nil {
+		return nil, errors.New("invalid IP address")
+	}
+
+	nPort, err := strconv.Atoi(port)
+	if err != nil {
+		return nil, errors.New("invalid port number")
+	}
+
+	if nPort == 0 {
+		nPort = defaultPort
+	}
+	return &net.TCPAddr{IP: ip, Port: nPort}, nil
 }
