30650e535ad241c96a722fdf156f6144e7fd32e2
Martin Angers <martin.n.angers@gmail.com>
2017-07-13 12:47:27 -0400

    fix sha display for the latest version

--- misc/scripts/version_sha.sh
+++ misc/scripts/version_sha.sh
@@ -2,7 +2,9 @@
 
 set -euo pipefail
 
+version=$(git describe --tags)
 srcArchivePath=$(mktemp -t regis-companion.XXXX)
+curl -o ${srcArchivePath} "https://github.com/harfangapps/regis-companion/archive/${version}.tar.gz"
 sha=$(shasum -a 256 ${srcArchivePath})
 echo "SHA256 for Homebrew formula: ${sha}"
 unlink ${srcArchivePath}
